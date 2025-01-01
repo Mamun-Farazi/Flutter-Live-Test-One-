5e9edb669587c63e7136592bc77edc53470444b1
Mamun-Farazi <mamunfarazi2019@gmail.com>
2025-01-01 23:23:17 +0600

    Solved: The problem area of triangle

--- bin/livetest_1.dart
+++ bin/livetest_1.dart
@@ -1,5 +1,17 @@
-
+import 'dart:io';
 
 void main(){
-  print("Hello World");
+  print("Enter Base For Triangle : ");
+  double baseOfTriangle = double.parse(stdin.readLineSync()!);
+  print("Enter Height For Triangle : ");
+  double heightOfTriangle = double.parse(stdin.readLineSync()!);
+
+  triangle(baseOfTriangle, heightOfTriangle);
+}
+
+void triangle(double base, double height){
+   double areaOfTriangle;
+
+   areaOfTriangle = 0.5*base*height;
+   print("Area of Triangle : ${areaOfTriangle.toStringAsFixed(2)}");
 }
